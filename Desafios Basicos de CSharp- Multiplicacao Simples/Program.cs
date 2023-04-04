63392239cb00f1f424fd0464415972883f09dbc1
carlosmg2005 <carlosmg2005@msn.com>
2023-04-04 15:27:21 -0300

    Adicionar arquivos de projeto.

---
+++ Desafios Basicos de CSharp- Multiplicacao Simples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafios_Basicos_de_CSharp__Multiplicacao_Simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("PROD = " + (a * b));
+            Console.ReadLine();
+        }
+    }
+}
